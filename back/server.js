c22d4c376e21ce65ca38632adc3a6aa3f04e6697
Kevin GANEM <Ganemkevin@gmail.com>
2022-03-14 12:22:44 +0100

    first commit

---
+++ back/server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const hotelsRouter = require("./router/hotelsRouter");
+const restaurantsRouter = require("./router/restaurantsRouter");
+const app = express();
+
+app.use(express.json());
+app.get("/", (_req, res) => {
+  res.send("...");
+});
+
+app.use("/hotels", hotelsRouter);
+app.use("/restaurants", restaurantsRouter);
+
+app.get("*", (_req, res) => {
+  res.status(404).send("error 404");
+});
+app.listen(8000, () => {
+  console.log("listening...");
+});
